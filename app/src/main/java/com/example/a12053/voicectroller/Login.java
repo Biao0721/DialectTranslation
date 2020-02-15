6dfe03fcdbcb4399ff07661dd5cbe3ec0dd427cc
biao0721 <877763610@qq.com>
2020-02-16 02:18:33 +0800

    修改登录界面

--- app/src/main/java/com/example/a12053/voicectroller/Login.java
+++ app/src/main/java/com/example/a12053/voicectroller/Login.java
@@ -9,12 +9,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Login extends AppCompatActivity {
+    List<Map<String, String>> user = new ArrayList<>();
 
     public static String name;
 
-    private Button btnLogin;
-    private EditText et;
+    private EditText usernameET;
+    private EditText passwordET;
+    private Button loginBtn;
 
     private static void setName(String _name){
         name = _name;
@@ -31,23 +38,75 @@ public class Login extends AppCompatActivity {
             actionBar.hide();
         }
 
-        btnLogin = findViewById(R.id.login);
-        et = findViewById(R.id.user);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        setUser();
+        init();
+    }
+
+    private void setUser(){
+        String[] accountNumber = {
+                "KonDBin", "DonShanYu", "XieZhuoJun", "LeeJie", "ChenMinHang"
+        };
+
+        String[] userName = {
+                "孔德彬", "董善宇", "谢卓君", "李捷", "陈明航"
+        };
+
+        String[] password = {
+                "123", "123", "123", "123", "123"
+        };
+
+        for(int i = 0; i < accountNumber.length; i++){
+            Map<String, String> userItem = new HashMap<>();
+            userItem.put("account_number", accountNumber[i]);
+            userItem.put("password", password[i]);
+            userItem.put("user_name", userName[i]);
+            user.add(userItem);
+        }
+    }
+
+    private void init(){
+        usernameET = findViewById(R.id.user);
+        passwordET = findViewById(R.id.password);
+        loginBtn   = findViewById(R.id.login);
+        setListeners();
+    }
+
+    private boolean login(){
+        String userText = usernameET.getText().toString();
+        String passwordText = passwordET.getText().toString();
+
+        for(int i = 0; i < user.size(); i++){
+            if(user.get(i).get("account_number").equals(userText) ){
+                if(user.get(i).get("password").equals(passwordText)){
+
+                    setName(user.get(i).get("user_name"));
+                    Toast.makeText(Login.this,"登录成功...",Toast.LENGTH_SHORT).show();
+
+                    return true;
+                }else{
+                    passwordET.setText("");
+                    Toast.makeText(Login.this,"密码错误...",Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            }else{
+                continue;
+            }
+        }
+        Toast.makeText(Login.this,"未找到用户...",Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    private void setListeners(){
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setName(et.getText().toString().trim());
-
-                if(name.isEmpty()){
-                    Toast.makeText(Login.this, "Name is empty",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(Login.this,"Successful Login",
-                            Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(Login.this,Settings.class);
+                if(login()){
+                    Intent intent = new Intent();
+                    intent.setClass(Login.this, Settings.class);
                     startActivity(intent);
                 }
             }
         });
     }
+
 }
